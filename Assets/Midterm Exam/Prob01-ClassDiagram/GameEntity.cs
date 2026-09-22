e5e92b2a06b23110e6f451d303bfc4f13f6a0fd1
WilliamTH-BU <william.smit@bumail.net>
2026-09-22 10:30:42 +0700

    Prob01 Full

--- Assets/Midterm Exam/Prob01-ClassDiagram/GameEntity.cs
+++ Assets/Midterm Exam/Prob01-ClassDiagram/GameEntity.cs
@@ -1,9 +1,27 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 namespace MidtermExam.Prob01
 {
     public class GameEntity
     {
         // TODO: Implement fields and methods according to Class Diagram
+        public string id;
+        private Vector3 position;
+        protected int health;
+
+        public virtual void Update()
+        {
+            
+        }
+        public virtual void TakeDamage(int damage)
+        {
+
+        }
+        private void Move(Vector3 direction)
+        {
+
+        }
     }
 }
